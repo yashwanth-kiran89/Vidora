6d44392c55c68109473d25bb2f812469fa95ecaf
Yashwanth <guvvayeshwanth@gmail.com>
2025-10-19 10:43:39 +0530

    Bind server to 0.0.0.0 for Render

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const initializeAndStartServer = async () => {
     })
 
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
+    app.listen(PORT,'0.0.0.0', () => {
       console.log(`Server running at http://localhost:${PORT}`)
     })
   } catch (e) {
